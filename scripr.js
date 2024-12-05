8cb705cff5a34bae877a16585c51bba9d60ed638
MASUR89 <surmava9@gmail.com>
2024-12-05 11:12:10 +0400

    setTimeout

--- scripr.js
+++ scripr.js
@@ -1,49 +1,56 @@
 let countdownTime = 10; 
 let remainingTime = countdownTime; 
 let intervalId = 0; 
+let timeoutId = 0; 
 
-const timerDisplay = document.getElementById("display");
-const messageDisplay = document.getElementById("message");
-const startButton = document.getElementById("startBut");
-const stopButton = document.getElementById("stopBut");
-const resetButton = document.getElementById("resetBut");
+const display = document.getElementById("display");
+const message = document.getElementById("message");
+const startBut = document.getElementById("startBut");
+const stopBut = document.getElementById("stopBut");
+const resetBut = document.getElementById("resetBut");
 
 function updateDisplay() {
-  timerDisplay.textContent = `Remaining ${remainingTime}s`;
+  display.textContent = `Remaining time: ${remainingTime}s`;
 }
 
 function startTimer() {
   if (intervalId !== 0) return;
 
-  messageDisplay.textContent = ""; 
+  message.textContent = ""; 
 
   intervalId = setInterval(() => {
     if (remainingTime > 0) {
       remainingTime--;
       updateDisplay();
-    } else {
-      stopTimer();
-      messageDisplay.textContent = "Time's up!";
     }
   }, 1000);
+
+  timeoutId = setTimeout(() => {
+    stopTimer(); 
+    message.textContent = "Time's up!";
+  }, remainingTime * 1050);
 }
 
 function stopTimer() {
   if (intervalId !== 0) {
     clearInterval(intervalId);
     intervalId = 0;
-    messageDisplay.textContent = `Timer stopped at ${remainingTime} seconds`;
   }
+  if (timeoutId !== 0) {
+    clearTimeout(timeoutId);
+    timeoutId = 0;
+  }
+  message.textContent = `Timer stopped at ${remainingTime} seconds`;
 }
 
 function resetTimer() {
-  remainingTime = countdownTime;
+  remainingTime = countdownTime; 
   updateDisplay();
-  messageDisplay.textContent = "";
+  message.textContent = ""; 
 }
 
-startButton.addEventListener("click", startTimer);
-stopButton.addEventListener("click", stopTimer);
-resetButton.addEventListener("click", resetTimer);
+startBut.addEventListener("click", startTimer);
+stopBut.addEventListener("click", stopTimer);
+resetBut.addEventListener("click", resetTimer);
 
 updateDisplay();
